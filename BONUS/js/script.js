9a1a05d2bf90769e00027ace7c3bce90008b661c
quintusdev <quintusdev92@gmail.com>
2023-06-08 15:29:52 +0200

    [UPLOAD] Upload Cartella Bonus - bonus 1 completato

---
+++ BONUS/js/script.js
@@ -0,0 +1,63 @@
+const {createApp} = Vue;
+
+//Implemento funzione createApp
+
+createApp({
+    data() {
+        return {
+            //Array Oggetti
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, 
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, 
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, 
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, 
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
+            //Creo un indice dove prende il primo elemento dell'array
+            activeImage: 0,
+        }
+    },
+    methods: {
+        changeImg(index){
+            this.activeImage = index;
+        },
+        // creo la funzione per andare avanti con le immagini e la collego al DOM del pulsante corrispondente
+        nextImg(){
+            // incremento di uno l'activeImger
+            this.activeImage++;
+            //Controllo se e' arrivato alla fine dell'array e lo faccio tornare all'inizio
+            if(this.activeImage > this.slides.length - 1){
+                this.activeImage = 0
+            }
+        },
+        // creo la funzione per andare indietro con le immagini e la collego al DOM del pulsante corrispondente
+        prevImg(){
+            // incremento di uno l'activeImger
+            this.activeImage--;
+            //Controllo se e' arrivato all'inizio dell'array e lo faccio tornare alla fine
+            if(this.activeImage < 0){
+                this.activeImage = this.slides.length - 1;
+            }
+        }
+    },
+}).mount('#app')
---
+++ BONUS/readme.md
@@ -0,0 +1,28 @@
+PROBLEMA:
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue. Nello zip allegato trovate le immagini, il file index.html, il file js con già l'array di oggetti impostato ed il file css.
+
+1. Inserisco l'array nel data() del JS;
+
+2. Inserisco fuori dal return un indice con valore 0;
+
+3. Creo dei Methods:
+    3.1- Inserisco la funzione per il cambio immagine in base alla posizione dell'array;
+    
+    3.2- Inserisco la funzione per scorrere in avanti l'img corrispondente all'indice      dell'array, inoltre metto un if che mi controlli se sono arrivato all'ultima immagine dell'array mi riporta l'indice stesso in posizione 0;
+
+    3.3- Inserisco la funzione per scorrere indietro l'img corrispondente all'indice      dell'array, inoltre metto un if che mi controlli se sono arrivato all'ultima immagine dell'array mi riporta l'indice stesso all'ultima posizione dell'array;
+
+4. Implemento nel DOM con BIND una classe che assegni la classe ACTIVE (vedi CSS) alle THUMB nel caso in cui la posizione dell'immagine visualizzata corrisponda all'indice dell'array; altrimenti non assegno nessuna classe;
+
+5. Implemento un ciclo V-FOR per scorrere tutte le immagini dell'array;
+
+
+BONUS
+1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
+
+1. Implementata funzione CHANGEIMG cambia direttamente l'immagine visualizzata nell'ITEM:
+
+2. 
